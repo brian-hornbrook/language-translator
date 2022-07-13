d789061178cfe2d76e1c070f62a64005aa4edbe9
brian-hornbrook <brian_hornbrook@outlook.com>
2022-07-13 14:21:31 -0500

    Saved data to CSV file

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-translations = {
-    "hello": "hola",
-    "thank you": "gracias",
-    "sorry": "lo siento"
-}
+import csv
+
+translations = {}
+
+with open("translations.csv", "r") as words:
+    reader = csv.DictReader(words, delimiter=",")
+    for line in reader:
+        english = line["English"].lower()
+        spanish = line["Spanish"].lower()
+        french = line["French"].lower()
+        translations[english] = [spanish, french]
 
 done = False
 
@@ -15,9 +21,9 @@ while not done:
         done = True
 
     elif word in translations:
-        for english, spanish in translations.items():
+        for english, translation in translations.items():
             if word == english:
-                print(english + " is " + spanish + " in spnaish")
+                print(f'{english} is {translation[0]} in spanish and {translation[1]} in french')
 
     else:
         print("sorry, word isn't in the dictionary")
